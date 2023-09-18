4d21239020eadfc8d581c1ff92fed73f5e3453df
Sagar Subedi <sagarxic@gmail.com>
2023-09-18 12:46:38 +0545

    Initial commit

---
+++ src/main/java/com/example/redditclone/service/AuthService.java
@@ -0,0 +1,50 @@
+package com.example.redditclone.service;
+
+
+import com.example.redditclone.dto.RegisterRequest;
+import com.example.redditclone.model.User;
+import com.example.redditclone.model.VerificationToken;
+import com.example.redditclone.repository.UserRepository;
+import com.example.redditclone.repository.VerificationTokenRepository;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+      private final PasswordEncoder passwordEncoder;
+
+     private final  UserRepository userRepository;
+     private final VerificationTokenRepository verificationTokenRepository;
+
+    @Transactional
+    public void signup(RegisterRequest registerRequest){
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        user.setCreated(Instant.now());
+        user.setEnabled(false);
+
+        userRepository.save(user);
+
+        String token = generateVerificationToken(user);
+
+    }
+
+    private String generateVerificationToken(User user) {
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+        verificationTokenRepository.save(verificationToken);
+
+        return token;
+    }
+}
